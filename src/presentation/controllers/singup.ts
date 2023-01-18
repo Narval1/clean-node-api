d528af8d8e5b0ff8cd151ccaf4e248e054fa139a
Matheus Martins <matheushsm51@gmail.com>
2023-01-18 17:41:40 -0300

    feat: add email verification on singupcontroller

--- src/presentation/controllers/singup.ts
+++ src/presentation/controllers/singup.ts
@@ -2,14 +2,26 @@ import { HttpRequest, HttpResponse } from '../protocols/http'
 import { MissingParamError } from '../errors/missingParamErrors'
 import { badRequest } from '../helpers/httpHelper'
 import { Controller } from '../protocols/controller'
+import { EmailValidator } from '../protocols/emailValidator'
+import { InvalidParamError } from '../errors/invalidParamError'
 
 export class SignUpController implements Controller {
+  private readonly _emailValidator: EmailValidator
+
+  constructor (emailValidator: EmailValidator) {
+    this._emailValidator = emailValidator
+  }
+
   handle (httpRequest: HttpRequest): HttpResponse {
     const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
     for (const field of requiredFields) {
       if (!httpRequest.body[field]) {
         return badRequest(new MissingParamError(field))
       }
+    }
+    const isValid = this._emailValidator.isValid(httpRequest.body.email)
+    if (!isValid) {
+      return badRequest(new InvalidParamError('email'))
     }
   }
 }
