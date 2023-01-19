9cfeb353900a332d30b0c7b1fddc8628fbedf33c
Matheus Martins <matheushsm51@gmail.com>
2023-01-19 15:45:37 -0300

    refactor: removed code duplication

--- src/presentation/controllers/singup.ts
+++ src/presentation/controllers/singup.ts
@@ -17,10 +17,11 @@ export class SignUpController implements Controller {
           return badRequest(new MissingParamError(field))
         }
       }
-      if (httpRequest.body.password !== httpRequest.body.passwordConfirmation) {
+      const { password, passwordConfirmation, email } = httpRequest.body
+      if (password !== passwordConfirmation) {
         return badRequest(new InvalidParamError('passwordConfirmation'))
       }
-      const isValid = this._emailValidator.isValid(httpRequest.body.email)
+      const isValid = this._emailValidator.isValid(email)
       if (!isValid) {
         return badRequest(new InvalidParamError('email'))
       }
