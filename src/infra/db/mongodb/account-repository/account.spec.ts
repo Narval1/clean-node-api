3053330536efb2a743e6db4491211539327308cf
Matheus Martins <matheushsm51@gmail.com>
2023-01-26 17:29:00 -0300

    refactor: moved AccountMongoRepository creation to a factory method

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -9,8 +9,12 @@ describe('account mongod repository', () => {
     await MongoHelper.disconnect()
   })
 
+  const makeSut = (): AccountMongoRepository => {
+    return new AccountMongoRepository()
+  }
+
   test('should return an account on success', async () => {
-    const sut = new AccountMongoRepository()
+    const sut = makeSut()
     const account = await sut.add({
       name: 'any_name',
       email: 'any_email@email.com',
